c2892a90c4935047698e5bc7e7461bd92576f292
unahceel <163097373+unahceel@users.noreply.github.com>
2024-10-11 17:02:38 +0900

    Update main.go

--- main.go
+++ main.go
@@ -8,3 +8,18 @@ func main() {
 
 
 }
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var army string = "우리는 !가와 !민에 충성을 다하는 대한민! 육군이다"
+	armyfixed := strings.NewReplacer("!", "국")
+	fmt.Println(army)
+	fmt.println(armyFixed.Peplace(army))
+
+}
